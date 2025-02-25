006e3e63a590c8be4f0643377f7adf0059cf71ee
Florent Drilhon <florentdrilhonvvs@gmail.com>
2025-02-25 13:11:43 +0100

    [DPE] add dpe color constants

---
+++ src/features/constants.ts
@@ -0,0 +1,55 @@
+// Constants for class A
+export const A_DPE_BACKGROUND = "#00A06D";
+export const A_DPE_COLOR = "#FFFFFF";
+
+// Constants for class B
+export const B_DPE_BACKGROUND = "#52B153";
+export const B_DPE_COLOR = "#FFFFFF";
+
+// Constants for class C
+export const C_DPE_BACKGROUND = "#A5CC74";
+export const C_DPE_COLOR = "#FFFFFF";
+
+// Constants for class D
+export const D_DPE_BACKGROUND = "#F4E70F";
+export const D_DPE_COLOR = "#000000";
+
+// Constants for class E
+export const E_DPE_BACKGROUND = "#F0B40F";
+export const E_DPE_COLOR = "#FFFFFF";
+
+// Constants for class F
+export const F_DPE_BACKGROUND = "#EB8235";
+export const F_DPE_COLOR = "#FFFFFF";
+
+// Constants for class G
+export const G_DPE_BACKGROUND = "#D7221F";
+export const G_DPE_COLOR = "#FFFFFF";
+
+// Constants for class A GES
+export const A_GES_BACKGROUND = "#BDE3FF";
+export const A_GES_COLOR = "#FFFFFF";
+
+// Constants for class B GES
+export const B_GES_BACKGROUND = "#A4DBF8";
+export const B_GES_COLOR = "#FFFFFF";
+
+// Constants for class C GES
+export const C_GES_BACKGROUND = "#8CB4D3";
+export const C_GES_COLOR = "#FFFFFF";
+
+// Constants for class D GES
+export const D_GES_BACKGROUND = "#7792B1";
+export const D_GES_COLOR = "#FFFFFF";
+
+// Constants for class E GES
+export const E_GES_BACKGROUND = "#606F8F";
+export const E_GES_COLOR = "#FFFFFF";
+
+// Constants for class F GES
+export const F_GES_BACKGROUND = "#4D5271";
+export const F_GES_COLOR = "#FFFFFF";
+
+// Constants for class G GES
+export const G_GES_BACKGROUND = "#393551";
+export const G_GES_COLOR = "#FFFFFF";
