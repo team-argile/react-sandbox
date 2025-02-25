a715a744c58c1243f53d46583d724a060791db75
Florent Drilhon <florentdrilhonvvs@gmail.com>
2025-02-25 13:33:39 +0100

    [logs] add warning on AdressInput

--- src/hooks/useFetchAddressSuggestions.ts
+++ src/hooks/useFetchAddressSuggestions.ts
@@ -2,6 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { searchAddressSuggestions } from "@/services/api";
 
 export const useFetchAddressSuggestions = (addressInput: string) => {
+  if (addressInput.length < 30) {
+    console.warn("Adress input should be at least 3 characters long");
+  }
+
   const { data, isLoading, isError } = useQuery({
     queryKey: ["fetchAddressSuggestions", addressInput],
     queryFn: () => searchAddressSuggestions(addressInput, 5),
