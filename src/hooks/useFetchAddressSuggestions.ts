9ec9fcbddbab7ac580351ee328b0999fdc2d4a2a
Florent Drilhon <florentdrilhonvvs@gmail.com>
2025-02-26 18:26:23 +0100

    [misc] improve code readability

--- src/hooks/useFetchAddressSuggestions.ts
+++ src/hooks/useFetchAddressSuggestions.ts
@@ -1,15 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { searchAddressSuggestions } from "@/services/api";
 
+const MIN_ADDRESS_LENGTH = 30;
+
 export const useFetchAddressSuggestions = (addressInput: string) => {
-  if (addressInput.length < 30) {
+  if (addressInput.length < MIN_ADDRESS_LENGTH) {
     console.warn("Adress input should be at least 3 characters long");
   }
 
   const { data, isLoading, isError } = useQuery({
     queryKey: ["fetchAddressSuggestions", addressInput],
     queryFn: () => searchAddressSuggestions(addressInput, 5),
-    enabled: !!addressInput && addressInput.length > 30,
+    enabled: !!addressInput && addressInput.length > MIN_ADDRESS_LENGTH,
     staleTime: 5 * 60 * 1000,
   });
 
